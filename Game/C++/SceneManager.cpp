892a24e0d576f6e192924b277dd225b75008d932
Jayansh Devgan <72650671+JayanshDevgan@users.noreply.github.com>
2025-01-13 21:17:14 +0530

    Project Setup

---
+++ Game/C++/SceneManager.cpp
@@ -0,0 +1,23 @@
+﻿#include "SceneManager.h"
+
+SceneManager::SceneManager(const SpawnParams& params)
+    : Script(params)
+{
+    // Enable ticking OnUpdate function
+    _tickUpdate = true;
+}
+
+void SceneManager::OnEnable()
+{
+    // Here you can add code that needs to be called when script is enabled (eg. register for events)
+}
+
+void SceneManager::OnDisable()
+{
+    // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+}
+
+void SceneManager::OnUpdate()
+{
+    // Here you can add code that needs to be called every frame
+}
---
+++ Game/C++/SceneManager.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "Engine/Scripting/Script.h"
+
+API_CLASS() class GAME_API SceneManager : public Script
+{
+API_AUTO_SERIALIZATION();
+DECLARE_SCRIPTING_TYPE(SceneManager);
+
+    // [Script]
+    void OnEnable() override;
+    void OnDisable() override;
+    void OnUpdate() override;
+};
